d59496d5bd66553a3fe46294ba5a6c71230a92cf
Marc Max MALKA <1161961+TheNoobCompany@users.noreply.github.com>
2018-11-10 18:39:32 +0100

    refactor(InventoryManager): Change the for into a foreach for added readability

--- src/GildedRose.Console/InventoryManager.cs
+++ src/GildedRose.Console/InventoryManager.cs
@@ -8,74 +8,74 @@ namespace GildedRose.Console
 
         public static void UpdateQuality()
         {
-            for (var i = 0; i < Inventory.Count; i++)
+            foreach (Item currentItem in Inventory)
             {
-                if (Inventory[i].Name != "Aged Brie" && Inventory[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (currentItem.Name != "Aged Brie" && currentItem.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Inventory[i].Quality > 0)
+                    if (currentItem.Quality > 0)
                     {
-                        if (Inventory[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (currentItem.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            Inventory[i].Quality = Inventory[i].Quality - 1;
+                            currentItem.Quality = currentItem.Quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    if (Inventory[i].Quality < 50)
+                    if (currentItem.Quality < 50)
                     {
-                        Inventory[i].Quality = Inventory[i].Quality + 1;
+                        currentItem.Quality = currentItem.Quality + 1;
 
-                        if (Inventory[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (currentItem.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Inventory[i].SellIn < 11)
+                            if (currentItem.SellIn < 11)
                             {
-                                if (Inventory[i].Quality < 50)
+                                if (currentItem.Quality < 50)
                                 {
-                                    Inventory[i].Quality = Inventory[i].Quality + 1;
+                                    currentItem.Quality = currentItem.Quality + 1;
                                 }
                             }
 
-                            if (Inventory[i].SellIn < 6)
+                            if (currentItem.SellIn < 6)
                             {
-                                if (Inventory[i].Quality < 50)
+                                if (currentItem.Quality < 50)
                                 {
-                                    Inventory[i].Quality = Inventory[i].Quality + 1;
+                                    currentItem.Quality = currentItem.Quality + 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (Inventory[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (currentItem.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Inventory[i].SellIn = Inventory[i].SellIn - 1;
+                    currentItem.SellIn = currentItem.SellIn - 1;
                 }
 
-                if (Inventory[i].SellIn < 0)
+                if (currentItem.SellIn < 0)
                 {
-                    if (Inventory[i].Name != "Aged Brie")
+                    if (currentItem.Name != "Aged Brie")
                     {
-                        if (Inventory[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (currentItem.Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Inventory[i].Quality > 0)
+                            if (currentItem.Quality > 0)
                             {
-                                if (Inventory[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (currentItem.Name != "Sulfuras, Hand of Ragnaros")
                                 {
-                                    Inventory[i].Quality = Inventory[i].Quality - 1;
+                                    currentItem.Quality = currentItem.Quality - 1;
                                 }
                             }
                         }
                         else
                         {
-                            Inventory[i].Quality = Inventory[i].Quality - Inventory[i].Quality;
+                            currentItem.Quality = currentItem.Quality - currentItem.Quality;
                         }
                     }
                     else
                     {
-                        if (Inventory[i].Quality < 50)
+                        if (currentItem.Quality < 50)
                         {
-                            Inventory[i].Quality = Inventory[i].Quality + 1;
+                            currentItem.Quality = currentItem.Quality + 1;
                         }
                     }
                 }
