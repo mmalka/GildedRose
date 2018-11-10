afe224cbab117c96ae005bd080f396d951210524
Marc Max MALKA <1161961+TheNoobCompany@users.noreply.github.com>
2018-11-10 20:17:30 +0100

    test(LegendaryItems): Implement Legendary item deducted business rule

---
+++ src/GildedRose.Tests/LegendaryItemsTest.cs
@@ -0,0 +1,38 @@
+﻿using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class LegendaryItemsTest
+    {
+        private const int ExpectedSellIn = 0;
+        private const int ExpectedQuality = 80;
+
+        private readonly LegendaryItem _expectedLegendaryItem = new LegendaryItem()
+        {
+            Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = ExpectedQuality
+        };
+
+        [Fact]
+        public void ShouldAlwaysKeepInitialQualityIfLegendary()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _expectedLegendaryItem.Update();
+            }
+
+            Assert.Equal(ExpectedQuality, _expectedLegendaryItem.Quality);
+        }
+
+        [Fact]
+        public void ShouldAlwaysKeepInitialSellInIfLegendary()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _expectedLegendaryItem.Update();
+            }
+
+            Assert.Equal(ExpectedSellIn, _expectedLegendaryItem.SellIn);
+        }
+    }
+}
